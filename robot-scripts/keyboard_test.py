8b564bb535ef92084ac45b41996c95395d432021
Kamil Kowalczyk <kamkow10@wp.pl>
2021-06-08 19:55:56 +0200

    fast update

--- robot-scripts/keyboard_test.py
+++ robot-scripts/keyboard_test.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     while True:
         try:
             keyboard.on_press_key('space', print('space'))
+            keyboard.wait()
             sleep(1)
         except KeyboardInterrupt:
             break
