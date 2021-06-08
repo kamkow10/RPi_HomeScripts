65090c07bdbc7f093e3fe96ed63fd0dfbb48c3cf
Kamil Kowalczyk <kamkow10@wp.pl>
2021-06-08 20:18:34 +0200

    fast update

--- robot-scripts/keyboard_test.py
+++ robot-scripts/keyboard_test.py
@@ -1,18 +1,5 @@
 import keyboard
-import string
-from threading import *
 
-
-keys = list(string.ascii_lowercase)
-
-
-def listen(key):
-    while True:
-        keyboard.wait(key)
-        print("[+] Pressed",key)
-
-
-if __name__ == '__main__':
-    threads = [Thread(target=listen, kwargs={"key":key}) for key in keys]
-    for thread in threads:
-        thread.start()
+# Check if b was pressed
+if keyboard.is_pressed('b'):
+	print('b Key was pressed')
