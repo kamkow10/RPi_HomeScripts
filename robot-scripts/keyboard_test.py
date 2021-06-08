562fc3c4a76d244ec0c42501a3edf92d7c98d449
Kamil Kowalczyk <kamkow10@wp.pl>
2021-06-08 20:13:49 +0200

    fast update

--- robot-scripts/keyboard_test.py
+++ robot-scripts/keyboard_test.py
@@ -1,20 +1,18 @@
-from pynput import keyboard
+import keyboard
+import string
+from threading import *
 
 
-def on_press(key):
-    if key == keyboard.Key.esc:
-        return False  # stop listener
-    try:
-        k = key.char  # single-char keys
-    except:
-        k = key.name  # other keys
-    if k in ['1', '2', 'left', 'right']:  # keys of interest
-        # self.keys.append(k)  # store it in global-like variable
-        print('Key pressed: ' + k)
-        return False  # stop listener; remove this if want more keys
+keys = list(string.ascii_lowercase)
+
+
+def listen(key):
+    while True:
+        keyboard.wait(key)
+        print("[+] Pressed",key)
 
 
 if __name__ == '__main__':
-    listener = keyboard.Listener(on_press=on_press)
-    listener.start()  # start to listen on a separate thread
-    listener.join()  # remove if main thread is polling self.keys
+    threads = [Thread(target=listen, kwargs={"key":key}) for key in keys]
+    for thread in threads:
+        thread.start()
