d40b598e720fc05e89ae5a100bb5cb29a1828700
Kamil Kowalczyk <kamkow10@wp.pl>
2021-06-08 19:59:54 +0200

    fast update

--- robot-scripts/keyboard_test.py
+++ robot-scripts/keyboard_test.py
@@ -6,8 +6,6 @@ if __name__ == '__main__':
     while True:
         try:
             keyboard.add_hotkey('space', lambda: print('space'))
-            keyboard.wait()
-            sleep(1)
         except KeyboardInterrupt:
             break
     print('end')
